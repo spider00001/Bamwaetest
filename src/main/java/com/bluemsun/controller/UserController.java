4713982e8d7afb95d94bc14aecbaaefb85ec69b5
高文智 <9061817+gao-wenzhi@user.noreply.gitee.com>
2022-05-16 20:19:54 +0800

    jenkins-test

---
+++ src/main/java/com/bluemsun/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.bluemsun.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "hello user";
+
+    }
+}
