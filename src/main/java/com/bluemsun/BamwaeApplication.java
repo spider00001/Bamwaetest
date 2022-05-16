c162ab6e2b80cc3603c5af27854857b26ffcf57e
高文智 <9061817+gao-wenzhi@user.noreply.gitee.com>
2022-05-16 10:26:06 +0800

    Initial commit

---
+++ src/main/java/com/bluemsun/BamwaeApplication.java
@@ -0,0 +1,12 @@
+package com.bluemsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamwaeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BamwaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bluemsun/BamwaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluemsun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamwaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
