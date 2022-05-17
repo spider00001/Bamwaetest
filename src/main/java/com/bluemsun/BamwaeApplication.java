e7e58bdd8877c7b8a045ffe563028269808ee35f
高文智 <9061817+gao-wenzhi@user.noreply.gitee.com>
2022-05-17 18:25:19 +0800

    server:   port: 8082   测试SSL

--- src/main/java/com/bluemsun/BamwaeApplication.java
+++ src/main/java/com/bluemsun/BamwaeApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BamwaeApplication {
     public static void main(String[] args) {
         SpringApplication.run(BamwaeApplication.class, args);
-
     }
 }
