c9b1bc5cdb069c7ef277999ac33848f25dae8ca6
高文智 <9061817+gao-wenzhi@user.noreply.gitee.com>
2022-05-17 21:03:42 +0800

    test测试1111

--- src/main/java/com/bluemsun/controller/UserController.java
+++ src/main/java/com/bluemsun/controller/UserController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @RequestMapping("/login")
-
-
     public String login() {
         return "hello user";
 
